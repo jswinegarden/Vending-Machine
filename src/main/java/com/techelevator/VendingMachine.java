e1e8b6cf1ef19a90c3ac46206eb0eaa6b549b24c
Jonathon Winegarden <jswinegarden@gmail.com>
2020-10-08 14:52:15 -0400

    Added transactions class

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,12 +1,20 @@
 package com.techelevator;
 
-public class VendingMachine {
+public class VendingMachine extends Transactions implements Accountable {
 
 	private int productAmount;
 	
-	public VendingMachine(int productAmount) {
+	public VendingMachine(int productAmount, int balance) {
+		super(balance);
 		this.productAmount = productAmount;
 	}
+	
+	@Override
+    public int transferTo(Transactions destination, int transferAmount) {
+    	destination.feedMoney(transferAmount);
+    	this.withdrawChange(transferAmount);
+    	return getBalance();
+    }
 	
 	public int stockInventory(int productToStock) {
 		productAmount += productToStock;
@@ -25,6 +33,10 @@ public class VendingMachine {
 	public void setProductAmount(int productAmount) {
 		this.productAmount = 5;
 	}
-	
+
+	@Override
+	public int getBalance() {
+		return 0;
+	}
 	
 }
